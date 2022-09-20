3668163338231408c9affa20c52fbd1c35a49571
Shehneel Khan <swali@intradyn.com>
2022-09-20 22:23:48 +0500

    Added CRUD operations

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HelloOrderView.as_view(), name='hello_orders'),
+    path('', views.OrderCreateListView.as_view(), name='orders'),
+    path('<int:order_id>/', views.OrderDetailView.as_view(), name='order_detail')
 ]
